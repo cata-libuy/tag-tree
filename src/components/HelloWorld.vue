cee0898e73f2ed89b339b4697a6062069cf3f9fb
Catalina Orellana <catalina.orellana@2brains.cl>
2018-04-27 11:54:54 -0300

    Gets tags and its relationships from a list of given links

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,95 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://github.com/vuejs/vue-cli/tree/dev/docs" target="_blank">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org/en/essentials/getting-started.html" target="_blank">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org/en/intro.html" target="_blank">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org/en" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <svg :width="width" :height="height">
+    </svg>
   </div>
 </template>
 
 <script>
+import * as _ from 'underscore'
+import Vue from 'vue'
+import VueResource from 'vue-resource'
+Vue.use(VueResource)
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data: function () {
+    return {
+      width: 900,
+      height: 400,
+      circlesData: [],
+      links: []
+    }
+  },
+  computed: {
+    tags () { // Obtiene los tags y sus relaciones
+      const tags = _.uniq(_.flatten(_.pluck(this.links, 'tags')))
+      const tagsData = _.map(tags, (tagName) => {
+        return {
+          label: tagName,
+          linksCount: _.reduce(this.links, (memo, link) => {
+            return memo + _.filter(link.tags, tag => tag === tagName).length
+          }, 0),
+          relatedTo: _.reduce(this.links, (memo, link) => {
+            if (link.tags.indexOf(tagName) > -1) {
+              return _.uniq(_.flatten([memo, link.tags]))
+            }
+            return memo
+          }, [])
+        }
+      })
+      return _.sortBy(tagsData, (tag) => tag.linksCount).reverse()
+    }
+  },
+  methods: {
+    getCanvasCenter (tag) {
+      return { x: this.width / 2, y: this.height / 2 }
+    },
+    renderTags () {
+      const getRadius = data => 30 * data.linksCount
+      const getY = index => 150
+      const getX = index => 50 * 3 * (index + 1)
+      const svg = this.$d3.select('svg')
+      svg.selectAll("circle")
+        .data(this.tags)
+          .enter()
+          .append("circle")
+          .attr('cy', (data, index) => getY(index))
+          .attr('cx', (data, index) => getX(index))
+          .attr('r', (data) => getRadius(data))
+      svg.selectAll("text")
+        .data(this.tags)
+          .enter()
+          .append("text")
+          .attr('y', (data, index) => getY(index))
+          .attr('x', (data, index) => getX(index))
+          .text((data) => data.label)
+          .attr('font-family', 'sans-serif')
+          .attr('font-size', '14px')
+          .attr('fill', 'yellow')
+    }
+  },
+  created: function () {
+  this.$http.get('data/links.json')
+    .then(
+      (res) => {
+        this.links = res.body ? res.body : []
+        console.log('links', this.links, 'tags', this.tags)
+        this.renderTags()
+      },
+      (err) => console.log(err)
+    )
+  },
+  mounted () {
+    this.$d3.selectAll('p').style('color', 'red')
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
+import VueD3 from 'vue-d3'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+Vue.use(VueD3)
 
 new Vue({
   render: h => h(App)
