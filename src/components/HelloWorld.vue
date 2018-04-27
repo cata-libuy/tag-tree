05f43f421601de68e3f1db9dc9706d6f38691d6c
Catalina Orellana <catalina.orellana@2brains.cl>
2018-04-27 13:40:22 -0300

    Posiciona tags en radios concentricos de acuerdo a frecuencia

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -19,7 +19,7 @@ export default {
   data: function () {
     return {
       width: 900,
-      height: 400,
+      height: 600,
       circlesData: [],
       links: []
     }
@@ -30,26 +30,66 @@ export default {
       const tagsData = _.map(tags, (tagName) => {
         return {
           label: tagName,
-          linksCount: _.reduce(this.links, (memo, link) => {
+          relationScore: _.reduce(this.links, (memo, link) => {
             return memo + _.filter(link.tags, tag => tag === tagName).length
-          }, 0),
+          }, 1),
           relatedTo: _.reduce(this.links, (memo, link) => {
             if (link.tags.indexOf(tagName) > -1) {
-              return _.uniq(_.flatten([memo, link.tags]))
+              return _.without(_.uniq(_.flatten([memo, link.tags])), tagName)
             }
             return memo
-          }, [])
+          }, []),
+          position: this.getCanvasCenter()
         }
       })
-      return _.sortBy(tagsData, (tag) => tag.linksCount).reverse()
+      return _.sortBy(tagsData, (tag) => tag.relationScore).reverse()
     }
   },
   methods: {
     getCanvasCenter (tag) {
-      return { x: this.width / 2, y: this.height / 2 }
+      return { x: parseInt(this.width / 2), y: parseInt(this.height / 2) }
     },
-    renderTags () {
-      const getRadius = data => 30 * data.linksCount
+    /* De acuerdo a importancia, posiciona la primera al centro
+    y las siguientes en puntos aleatorios equidistantes al centro de acuerdo a su importancia.
+    TODO: generar distintos centros para tags no relacionadas */
+    updateTagsPositions () {
+      const center = this.getCanvasCenter()
+      let lastWeight = this.tags[0].relationScore
+      let distanceToCenter = 0
+      const growStep = 150
+      _.each(this.tags, (tag, index) => {
+        if (index === 0) {
+          return tag.position = center
+        }
+        if (tag.relationScore < lastWeight) {
+          distanceToCenter = distanceToCenter + growStep
+        }
+        lastWeight = tag.relationScore
+        const min = 0
+        const max = 2 * Math.PI
+        let thita = Math.random() * (max - min) + min
+        // let thita = Math.PI/4
+        console.log('thita for', tag.label, thita)
+        tag.position = {
+          x: thita >= 0 && thita < Math.PI / 2 // estoy en cuadrante 1
+            ? parseInt(center.x + Math.cos(thita) * distanceToCenter)
+            : thita ===  Math.PI / 2 || thita === 3 * Math.PI / 2 // estoy en eje y, x no se mueve
+              ? center.x
+              : thita < 3 * Math.PI / 2 // estoy en cuadrantes 2 o 3
+                ? parseInt(center.x - Math.cos(thita) * distanceToCenter)
+                : parseInt(center.x + Math.cos(thita) * distanceToCenter),
+          y: thita > 0 && thita < Math.PI // cuadrantes 1 o 2, resto altura
+              ? parseInt(center.y - Math.sin(thita) * distanceToCenter)
+              : thita == 0 || thita == Math.PI || thita == 2 * Math.PI
+                ? parseInt(center.y)
+                : parseInt(center.y + Math.sin(thita) * distanceToCenter) // cuadrantes 3 o 4, sumo altura
+        }
+        console.log(tag.label, tag.position)
+        return
+      })
+    },
+    renderTagsAligned () {
+      const getRadius = data => 15 + 10 * data.relationScore
       const getY = index => 150
       const getX = index => 50 * 3 * (index + 1)
       const svg = this.$d3.select('svg')
@@ -66,6 +106,29 @@ export default {
           .append("text")
           .attr('y', (data, index) => getY(index))
           .attr('x', (data, index) => getX(index))
+          .text((data) => data.label)
+          .attr('font-family', 'sans-serif')
+          .attr('font-size', '14px')
+          .attr('fill', 'yellow')
+    },
+    renderTags () {
+      this.updateTagsPositions()
+      console.log(this.tags)
+      const svg = this.$d3.select('svg')
+      const getRadius = data => 15 + 10 * data.relationScore
+      svg.selectAll("circle")
+        .data(this.tags)
+          .enter()
+          .append("circle")
+          .attr('cy', (data) => data.position.y)
+          .attr('cx', (data) => data.position.x)
+          .attr('r', (data) => getRadius(data))
+      svg.selectAll("text")
+        .data(this.tags)
+          .enter()
+          .append("text")
+          .attr('y', (data) => data.position.y)
+          .attr('x', (data) => data.position.x)
           .text((data) => data.label)
           .attr('font-family', 'sans-serif')
           .attr('font-size', '14px')
